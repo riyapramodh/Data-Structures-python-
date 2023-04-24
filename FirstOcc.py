51f19c7293d466db4db08018c9f97d64e306d8d2
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-04-24 21:46:40 +0530

    Update FirstOcc.py

--- FirstOcc.py
+++ FirstOcc.py
@@ -4,14 +4,14 @@ def func(arr,x,n):
     while low <= high:
         mid = low + high // 2
         if arr[mid] < x:
-            low = mid - 1
+            low = mid + 1
         elif arr[mid] > x:
-            high = mid + 1
+            high = mid - 1
         else:
             if mid == 0 or arr[mid - 1] != arr[mid]:
                 return mid
             else:
-                high = mid + 1
+                high = mid - 1
 
         return -1
 
