5767ae632606600e050746b2e474b50a88bf33da
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-04-24 21:47:42 +0530

    Update LastOcc.py

--- LastOcc.py
+++ LastOcc.py
@@ -4,14 +4,14 @@ def func(arr,x,n):
     while low <= high:
         mid = low + high // 2
         if arr[mid] < x:
-            low = mid - 1
+            low = mid + 1
         elif arr[mid] > x:
-            high = mid + 1
+            high = mid - 1
         else:
             if mid != n-1 or arr[mid] != arr[mid+1]:
                 return mid
             else:
-                high = mid + 1
+                high = mid - 1
 
         return -1
 
