f9f6f35c99f3ca7184d5eeac39ce680e5baa7ccf
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-04-28 16:41:08 +0530

    Update InsertionSort.py

--- InsertionSort.py
+++ InsertionSort.py
@@ -9,3 +9,10 @@ def insertionsort(arr,n):
         arr[j+1] = key #initially key is the first index but after completioin of first sort then the index position gets incremented by 1
     return arr
 print(insertionsort([20,5,40,60,10,30],6))
+
+
+"""
+the loop runs from the first element to the last element
+explanation:first we assign the first element as the key and store the previous element in a variable j
+the while loop is executed under the condition that it runs as long as the jth element is greater than 0 and the prev element to the key is greater than key and if it satisfies the condition it enters into the loop else it goes to the part where the key is getting overwritten by its own self 
+if the condition in the while loop would have been satisfied then the key element value gets overwritten by the value of the previous element j and the index of j will go one unit backwards and then when it tries to execute the loop of while again :if j  = 0 it will beocome j = j-1 = -1 and wont satisfy the first cndition or else in some other case it mit not satisfy the second condition and comes out of the loop and the next element becomes the key 
