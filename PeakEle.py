6847a2f4ccea4bcbc909c03e408c6eb2bb8e2099
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-05-31 19:19:16 +0530

    Update PeakEle.py

--- PeakEle.py
+++ PeakEle.py
@@ -10,26 +10,24 @@ def naive(arr,n):
             return arr[i]
     return -1
 
-def search(arr,n):
-    low = 0
-    high = n-1
-    mid = (low+high)//2
-    while low<= high:
-        if arr[low]>arr[1]:
-            return arr[low]
-        if arr[high]>arr[n-2]:
-            return arr[high]
-        if arr[mid]>arr[mid-1]and arr[mid]>arr[mid+1]:
-            return arr[mid]
-        if arr[mid-1]>arr[mid]:
-            high = mid -1
-        else:
-            low = mid + 1
-        if arr[mid]<arr[mid + 1]:
-            low = mid + 1
-        else:
-            high = mid -1
-    return -1
+    def bsearch(arr,n):
+        low = 0
+        high = n-2
+        if n==1:
+            return 0
+        if arr[0]>arr[1]:
+            return 0
+        if arr[n-1]>arr[n-2]:
+            return n-1
+        while low<=high:
+            mid = (low+high)//2
+            if arr[mid+1]>arr[mid]:
+                low = mid+1
+            elif arr[mid-1]>arr[mid]:
+                high = mid -1
+            else:
+                return mid
+        return -1
 
 print(naive([10,7,8,12,20],5))
 print(search([10,7,8,12,20],5))
