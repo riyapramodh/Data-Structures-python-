1776083ec14d8b8e20357c7f4f43325fc40d94e6
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-07-30 16:18:18 +0530

    Create Srach2DMatrix.py

---
+++ Srach2DMatrix.py
@@ -0,0 +1,56 @@
+#if the rows and columns of the matrix is not sorted - RECOMMENDED TO USE
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+
+        rows, cols = len(matrix), len(matrix[0])
+        row, col = 0, cols - 1
+
+        while row < rows and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                row += 1
+            else:
+                col -= 1
+
+        return False
+
+
+
+
+
+#if the rows and columns of the matrix are sorted
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix or not matrix[0]:
+            return False
+
+        rows = len(matrix)
+        cols = len(matrix[0])
+        left = 0
+        right = rows * cols - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            mid_val = matrix[mid // cols][mid % cols]
+
+            if target == mid_val:
+                return True
+            elif target < mid_val:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return False
