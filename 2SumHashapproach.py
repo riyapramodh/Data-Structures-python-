9a84482429f5beef067ab731d7e4b4d8f6f8ee6a
Riya Pramodh <112382228+riyapramodh@users.noreply.github.com>
2023-08-05 14:40:10 +0530

    Update 2SumHashapproach.py

--- 2SumHashapproach.py
+++ 2SumHashapproach.py
@@ -1,9 +1,19 @@
 #hash table method to see if the sum of two num in an array is eq to target:
-def check(arr,target):
-    hash = {}
-    for i,num in enumerate(arr): #i is the index and num is the ele at that index --> in this  manner we will apend it into the hash table
-        x = target - num
-        if x in hash:
-            return [hash[num],i]
-        hash[num] = i
-    return []
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        hasht = {}
+        for i, num in enumerate(nums):
+            x = target - num
+            if x in hasht:
+                return [hasht[x],i]
+            else:
+                hasht[num] = i
+            
+        return []
+
